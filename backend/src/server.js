07f92595058f90305841e8ec802107002fa25acf
SWADHIN300 <swadhinraha81@gamil.com>
2025-11-13 16:26:29 +0530

    solve all the bugs

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,21 @@ import express from "express";
 import { ENV } from "./lib/env.js";
 import path from "path";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 
 const app = express();
 
 const __dirname = path.resolve();
 
 app.use(express.json());
+app.use(cors());
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Content-Security-Policy",
+//     "default-src 'none'; connect-src 'self' http://localhost:3000; script-src 'self'; style-src 'self';"
+//   );
+//   next();
+// });
 
 app.get('/home',(req,res)=>{
     res.status(200).json({
@@ -16,10 +25,10 @@ app.get('/home',(req,res)=>{
 })
 
 //make our app ready for deployment
-if(ENV.NODE_ENV === "production"){
+if(ENV.NODE_ENV === "development"){
      app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*",(req,res)=>{
+    app.get("/{*any}",(req,res)=>{
         res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
     })
 }
