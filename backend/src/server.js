b36f8d665a77fbabb02627d4cc4fd753de8f9813
SWADHIN300 <swadhinraha81@gamil.com>
2025-11-15 00:42:27 +0530

    cuts the extra thing

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,15 +10,14 @@ const app = express();
 
 const __dirname = path.resolve();
 
-// middleware
+
 app.use(express.json());
-// credentials:true meaning?? => server allows a browser to include cookies on request
+
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
-app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
+
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
-app.use("/api/chat", chatRoutes);
-app.use("/api/sessions", sessionRoutes);
+
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
